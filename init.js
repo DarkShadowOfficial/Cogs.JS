36b71a377b16f0bcfe56060b678cce175762a77c
Volatile <95543040+DarkShadowOfficial@users.noreply.github.com>
2024-08-27 09:59:31 -0500

    Delete init.js

--- init.js
+++
@@ -1,9 +0,0 @@
-function addScript(src) {
-  let script = document.createElement('script');
-  script.src = src;
-  document.head.appendChild(script);
-}
-addScript("https://raw.githubusercontent.com/DarkShadowOfficial/Cogs.JS/main/Scripts/Algorithms.js");
-addScript("https://raw.githubusercontent.com/DarkShadowOfficial/Cogs.JS/main/Scripts/Statistics.js");
-addScript("https://raw.githubusercontent.com/DarkShadowOfficial/Cogs.JS/main/Scripts/Distribution.js");
-addScript("https://raw.githubusercontent.com/DarkShadowOfficial/Cogs.JS/main/Scripts/Regression.js");
